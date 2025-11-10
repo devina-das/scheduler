7536e16540e246374f89238b53cea38712fb9545
Devina Das <devinadas@wirelessprv-10-195-211-44.near.illinois.edu>
2025-11-10 13:54:18 -0600

    changed tasks to vec

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,57 @@
 use indexmap::IndexMap;
 
+// repps the different days of the week
 #[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
 pub enum DayOfWeek {
     Mon, Tue, Wed, Thu, Fri, Sat, Sun
 }
 
-// time can be repped as 23.59 for 23:59
+pub enum TaskAtb {
+    Title, Time, Desc
+}
+
+// title is basically a quick description
+// time is when it takes place. 24-hour clock and 23:59 is repped as 23.59
+// desc is a more in-depth description
+#[derive(Debug, Clone)]
 struct Task {
+    id: usize,
     title: String,
     time: f64,
     desc: String,
 }
 
-// I was using a HashMap at first, but those are unordered.
-// IndexMap is ordered
+// was using HashMap at first, but did some searching online and IndexMap
+// seems better because it's ordered.
 pub struct List {
     next_id: usize,
-    schedule: IndexMap<DayOfWeek, IndexMap<usize, Task>>
+    schedule: IndexMap<DayOfWeek, Vec<Task>>
 }
 
 impl Task {
-    pub fn display(&self){
-        println!("{} @ {}", self.title, self.time);
+    // displays the task like this:
+    //      #0 - Task 1 @ 5.45
+    //                This is our first task
+    pub fn display(&self) {
+        println!("#{} - {} @ {}", self.id, self.title, self.time);
         print!("          ");
         println!("{}", self.desc);
     }
 }
 
 impl List {
+
+    // makes a new list
+    // this is lowkey jank i know
     pub fn new() -> List {
-        let mut week: IndexMap<DayOfWeek, IndexMap<usize, Task>> = IndexMap::new();
-        week.insert(DayOfWeek::Mon, IndexMap::new());
-        week.insert(DayOfWeek::Tue, IndexMap::new());
-        week.insert(DayOfWeek::Wed, IndexMap::new());
-        week.insert(DayOfWeek::Thu, IndexMap::new());
-        week.insert(DayOfWeek::Fri, IndexMap::new());
-        week.insert(DayOfWeek::Sat, IndexMap::new());
-        week.insert(DayOfWeek::Sun, IndexMap::new());
+        let mut week: IndexMap<DayOfWeek,Vec<Task>> = IndexMap::new();
+        week.insert(DayOfWeek::Mon, Vec::new());
+        week.insert(DayOfWeek::Tue, Vec::new());
+        week.insert(DayOfWeek::Wed, Vec::new());
+        week.insert(DayOfWeek::Thu, Vec::new());
+        week.insert(DayOfWeek::Fri, Vec::new());
+        week.insert(DayOfWeek::Sat, Vec::new());
+        week.insert(DayOfWeek::Sun, Vec::new());
 
         List {
             next_id : 0,
@@ -44,36 +59,46 @@ impl List {
         }
     }
 
+    // adds a new task to the list in order of time
     pub fn add_task(&mut self, day: DayOfWeek, title: String, time: f64, desc: String) {
         if !check_time(time) {
             println!("invalid time");
             return;
         }
-        let new_task = Task {title, time, desc};
+        let id = self.next_id;
+        let new_task = Task {id, title, time, desc};
         self.schedule.get_mut(&day).unwrap().insert(self.next_id, new_task);
         self.order_tasks();
         self.next_id += 1;
     }
 
-    pub fn remove_task(&mut self, target: usize) {
+
+    // removes a task by the specified target id
+    pub fn remove_task(&mut self, target_id: usize) {
         for (_, tasks) in self.schedule.iter_mut() {
-            if tasks.shift_remove(&target).is_some() {
-                return;
+            for idx in 0..tasks.len() {
+                if tasks[idx].id == target_id {
+                    tasks.remove(idx);
+                    return;
+                }
             }
         }
     }
 
-    pub fn edit_task() {
+
+    // edits the specifiec task (by target_id)
+    // should also re-order if time is edited
+    pub fn edit_task(target_id: usize, to_edit: TaskAtb) {
         todo!();
     }
 
+
+    // displays the entire list
     pub fn display(&self) {
-        println!("/////////////////////////////////////////");
+        println!("----------------------------------------");
         for (day, tasks) in &self.schedule {
             println!("{}", to_string(*day));
-            for (id, task) in tasks {
-                print!("     ");
-                print!("#{} - ", id);
+            for task in tasks {
                 task.display();
             }
         }
@@ -82,12 +107,12 @@ impl List {
     // orders tasks by time
     fn order_tasks(&mut self) {
         for (_, tasks) in self.schedule.iter_mut() {
-            for i in 0..tasks.len() {
-                for j in 0..tasks.len() - i - 1 {
-                    let t1 = tasks.get_index(j).unwrap().1.time;
-                    let t2 = tasks.get_index(j + 1).unwrap().1.time;
-                    if t1 > t2 {
-                        tasks.swap_indices(j, j + 1); 
+            for swapped in 0..tasks.len() {
+                for idx in 0..tasks.len() - swapped - 1 {
+                    if tasks[idx].time > tasks[idx + 1].time {
+                        let temp = tasks[idx].clone();
+                        tasks[idx] = tasks[idx + 1].clone();
+                        tasks[idx + 1] = temp;
                     }
                 }
             }
@@ -95,12 +120,15 @@ impl List {
     }
 }
 
+
+// checks if the time is valid
 fn check_time(time: f64) -> bool {
     let int = time.floor();
     let dec = time - int;
     int >= 0.00 && int < 24.00 && dec >= 0.00 && dec < 0.60 
 }
 
+// turns DayOfWeek into equivalent string
 fn to_string(day: DayOfWeek) -> String {
     let day_string = match day {
         DayOfWeek::Mon => String::from("Monday"),
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
-use schedule::List;
+use schedule::{DayOfWeek, List};
 
 fn main() {
+    let mut new_list = List::new();
+
+    // task 1
+    let day1 = DayOfWeek::Mon;
+    let title1 = String::from("Task 1");
+    let time1 = 05.45;
+    let desc1 = String::from("This is our first task");
+    new_list.add_task(day1, title1, time1, desc1);
+    new_list.display();
+
+
+    // task 2
+    let day2 = DayOfWeek::Mon;
+    let title2 = String::from("Task 2");
+    let time2 = 02.45;
+    let desc2 = String::from("This is our second task");
+    new_list.add_task(day2, title2, time2, desc2);
+    new_list.display();
+
+    // remove task
+    new_list.remove_task(1);
+    new_list.display();
+
+
+
+
 
 }
